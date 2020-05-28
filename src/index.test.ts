848eab27919d8e4ec37ce9103ef5feaa2e2350c1
merisbahti <meris.bahti@gmail.com>
2020-05-28 18:51:29 +0000

    Implement usePartialSetState, add some tests.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,48 @@
-test('stuff', () => {
-  it('should work', () => {
-    expect(5).toBe(10)
+import { renderHook, act } from '@testing-library/react-hooks'
+import { createPartialSetState } from './index'
+import { useState } from 'react'
+import { optic } from 'optics-ts'
+
+test('should work with objects and arrays', () => {
+  type topState = {
+    a: { b: Array<number> }
+  }
+
+  const initialState: topState = { a: { b: [1, 2, 3] } }
+  const myOptic = optic<topState>()
+    .prop('a')
+    .prop('b')
+    .elems()
+
+  const { result: stateResult } = renderHook(() => useState(initialState))
+  const partialSetState = createPartialSetState(stateResult.current[1], myOptic)
+
+  act(() => {
+    partialSetState(oldValue => oldValue + 5)
   })
+
+  expect(stateResult.current[0]).toStrictEqual({
+    a: { b: [1, 2, 3].map(x => x + 5) },
+  })
+})
+
+test('should work with objects and arrays', () => {
+  type topState = {
+    a: { b: Array<number> }
+  }
+
+  const initialState: topState = { a: { b: [1, 2, 3] } }
+  const myOptic = optic<topState>()
+    .prop('a')
+    .prop('b')
+    .index(10)
+
+  const { result: stateResult } = renderHook(() => useState(initialState))
+  const partialSetState = createPartialSetState(stateResult.current[1], myOptic)
+
+  act(() => {
+    partialSetState(oldValue => oldValue + 5)
+  })
+
+  expect(stateResult.current[0]).toStrictEqual(initialState)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,32 @@
-const a: number = 5
-export default a
+import * as O from 'optics-ts'
+import { useCallback } from 'react'
+import { Dispatch, SetStateAction } from 'react'
+
+type SetState<S> = Dispatch<SetStateAction<S>>
+
+export const usePartialSetState = <S, A>(
+  setState: SetState<S>,
+  optic:
+    | O.Lens<S, any, A>
+    | O.Prism<S, any, A>
+    | O.Iso<S, any, A>
+    | O.Traversal<S, any, A>
+    | O.Equivalence<S, any, A>,
+): SetState<A> =>
+  useCallback(usePartialSetState(setState, optic), [setState, optic])
+
+export const createPartialSetState = <S, A>(
+  setState: SetState<S>,
+  optic:
+    | O.Lens<S, any, A>
+    | O.Prism<S, any, A>
+    | O.Iso<S, any, A>
+    | O.Traversal<S, any, A>
+    | O.Equivalence<S, any, A>,
+): SetState<A> => updater =>
+  setState(
+    O.modify(optic)(oldValue =>
+      updater instanceof Function ? updater(oldValue) : updater,
+    ),
+  )
+
